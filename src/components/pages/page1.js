092302021f9d24c4c07a84c8cc7b10f85540c40e
kasuyan <kasuya725@gmail.com>
2018-07-09 17:38:58 +0900

    eslint追加

--- src/components/pages/page1.js
+++ src/components/pages/page1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default class Page1 extends React.Component {
   render() {
--- src/components/pages/page2.js
+++ src/components/pages/page2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default class Page2 extends React.Component {
   render() {
