6622795673fd05bf1951c7cb23c0bcbbb39888e7
kasuyan <kasuya725@gmail.com>
2018-07-09 17:56:55 +0900

    eslintignore追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 module.exports = {
   entry: {
-    module: "./src/index.js"
+    module: './src/index.js'
   },
   output: {
-    path: __dirname + "/dist/js",
-    publicPath: "/js/",
-    filename: "main.js"
+    path: __dirname + '/dist/js',
+    publicPath: '/js/',
+    filename: 'main.js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         },
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   devServer: {
-    contentBase: "dist/",
+    contentBase: 'dist/',
     watchContentBase: true,
     inline: true,
-    host: "127.0.0.1",
+    host: '127.0.0.1',
     port: 3001
   }
 };
